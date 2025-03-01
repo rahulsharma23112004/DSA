a488ccdba9ac3761f13ca55fbc399eef009d66fc
Rahul Sharma <rahulsrma2311@gmail.com>
2025-03-01 12:27:49 +0530

    Create palindrome_using_recursion.cpp

---
+++ palindrome_using_recursion.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+bool checkPalindrome(string& str, int i,int j){
+    if(i>j){
+        return true;
+    }
+    if(str[i]!=str[j]){
+        return false;
+    }
+    else{
+        checkPalindrome(str,i+1,j-1);
+    }
+}
+
+int main()
+{
+    string name = "abcds";
+    bool isPalindrome = checkPalindrome(name , 0, name.length()-1);
+    if(isPalindrome){
+        cout<<"Its a Palindrome."<<endl;
+    }
+    else{
+        cout<<"Its not a palindrome"<<endl;
+    }
+    return 0;
+}
