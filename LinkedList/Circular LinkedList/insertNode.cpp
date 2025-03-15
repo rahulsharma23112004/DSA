ecb4fab6df7dbf2e87c38313ce3caa3a73d6185b
Rahul Sharma <rahulsrma2311@gmail.com>
2025-03-15 09:16:13 +0530

    Create insertNode.cpp

---
+++ LinkedList/Circular LinkedList/insertNode.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertNode(Node* &tail , int element , int d)
+{
+    if(tail==NULL){
+        Node* temp = new Node(d);
+        tail = temp;
+        temp->next = temp;
+    }
+    else{
+        Node* curr = tail;
+        while(curr->data!=element){
+            curr = curr->next;
+        }
+        Node* temp = new Node(d);
+        temp->next = curr->next;
+        curr->next = temp;
+    }
+}
+
+void print(Node* &tail)
+{
+    Node* temp = tail;
+    if(tail==NULL){
+        cout<<"List is empty."<<endl;
+        return;
+    }
+    do{
+        cout<<tail->data<<" ";
+        tail= tail->next;
+    }while(tail!=temp);
+    cout<<endl;
+}
+
+int main()
+{
+    Node* tail = NULL;
+    print(tail);
+    insertNode(tail, 5 ,3);
+    print(tail);
+    insertNode(tail,3,5);
+    print(tail);
+    insertNode(tail,5,7);
+    print(tail);
+    insertNode(tail,7,9);
+    print(tail);
+    return 0;
+}
