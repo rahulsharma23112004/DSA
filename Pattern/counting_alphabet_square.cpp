14523a87aa347e722c2d9a520391f61cb98a296c
Rahul Sharma <rahulsrma2311@gmail.com>
2025-03-23 09:25:27 +0530

    Create counting_alphabet_square.cpp

---
+++ Pattern/counting_alphabet_square.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n ;
+    char ch = 'A' ;
+    for(int i=1 ;i<=n ;i++){
+        for(int j=1;j<=n ;j++){
+            cout<<ch<<" " ;
+            ch++ ;
+
+        }
+        cout<<endl;
+    }
+    return 0;
+}
