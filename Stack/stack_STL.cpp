cdb546c3e3fe522317055f21a9f9654b0fe3550a
Rahul Sharma <rahulsrma2311@gmail.com>
2025-04-02 19:32:22 +0530

    Create stack_STL.cpp

---
+++ Stack/stack_STL.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+   stack<int> s;
+   s.push(2);
+   s.push(3);
+   s.pop();
+   cout<<"Top element: "<<s.top()<<endl;
+   if(s.empty()){
+    cout<<"Stack is empty."<<endl;
+   }
+   else{
+    cout<<"Stack is not empty."<<endl;
+   }
+   cout<<"Size of stack is: "<<s.size()<<endl;
+   return 0;
+}
