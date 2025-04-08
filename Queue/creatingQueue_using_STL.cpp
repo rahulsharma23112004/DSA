ceb7c6338d27b7f9fac944c9648e84f13888109f
Rahul Sharma <rahulsrma2311@gmail.com>
2025-04-08 09:02:55 +0530

    Create creatingQueue_using_STL.cpp

---
+++ Queue/creatingQueue_using_STL.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    queue<int> q;
+    q.push(11) ;
+    q.push(13);
+    q.push(15);
+
+    cout<<"Size of queue is: "<<q.size()<<endl;
+
+    q.pop();
+    q.pop();
+
+    cout<<"Element at front in queue is: "<<q.front()<<endl;
+
+    if(q.empty()){
+        cout<<"Queue is empty."<<endl;
+    }
+    else{
+        cout<<"Queue is not empty."<<endl;
+    }
+    return 0;
+}
