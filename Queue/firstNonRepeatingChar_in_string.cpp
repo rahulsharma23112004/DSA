959b41ed95f2901711b72caf1e38c3a74ecef5c3
Rahul Sharma <rahulsrma2311@gmail.com>
2025-04-14 08:10:15 +0530

    Create firstNonRepeatingChar_in_string.cpp

---
+++ Queue/firstNonRepeatingChar_in_string.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <map>
+using namespace std;
+
+string firstNonRepeating(string A)
+{
+    map<char, int> count;
+    string ans = "";
+    queue<int> q;
+
+    for (int i = 0; i < A.length(); i++)
+    {
+        char ch = A[i];
+
+        count[ch]++;
+
+        q.push(ch);
+
+        while (!q.empty())
+        {
+            if (count[q.front()] > 1)
+            {
+                q.pop();
+            }
+            else
+            {
+                ans.push_back(q.front());
+                break;
+            }
+        }
+
+        if (q.empty())
+        {
+            ans.push_back('#');
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    string A = "aabc";
+    string ans = firstNonRepeating(A);
+    cout << "First Non Repeating Character in string: " << ans << endl;
+    return 0;
+}
