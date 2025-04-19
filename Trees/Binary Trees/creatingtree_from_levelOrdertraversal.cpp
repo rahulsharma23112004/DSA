b9d75df8448633a171b5f8c2da331059ac25e01d
Rahul Sharma <rahulsrma2311@gmail.com>
2025-04-19 07:51:33 +0530

    Create creatingtree_from_levelOrdertraversal.cpp

---
+++ Trees/Binary Trees/creatingtree_from_levelOrdertraversal.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void buildFromLevelOrder(Node* root){
+    queue<Node*> q;
+    cout<<"Enter data for root: "<<endl;
+    int data;
+    cin>>data;
+
+    root = new Node(data);
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        cout<<"Enter data for left of " <<temp->data<<endl;
+        int leftData;
+        cin>>leftData;
+
+        if(leftData!=-1){
+            temp->left = new Node(leftData);
+            q.push(temp->left);
+        }
+
+
+        cout<<"Enter data for right of " <<temp->data<<endl;
+        int rightData;
+        cin>>rightData;
+
+        if(rightData!=-1){
+            temp->right = new Node(rightData);
+            q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+   Node* root = NULL;
+   buildFromLevelOrder(root);
+   return 0;
+}
