ee04bf34ee32986104a50b563fa2c2227052c9fd
Rahul Sharma <rahulsrma2311@gmail.com>
2025-04-26 10:11:25 +0530

    Create traversals.cpp

---
+++ Trees/Binary Trees/traversals.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* buildTree(Node* root){
+    cout<<"Enter data: "<<endl;
+    int data;
+    cin>>data;
+    
+    if(data == -1){
+        return NULL;
+    }
+
+    root = new Node(data);
+
+    cout<<"Enter data to insert in left of "<<data<<endl;
+    root->left = buildTree(root->left);
+
+    cout<<"Enter data to insert in right of "<<data<<endl;
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void inorder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+    
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root){
+    if(root == NULL){
+        return ;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main()
+{
+   Node* root = NULL;
+   root = buildTree(root);
+
+   cout<<"Inorder Traversal: ";
+   inorder(root);
+   cout<<endl;
+
+   cout<<"Preorder Traversal: ";
+   preorder(root);
+   cout<<endl;
+
+   cout<<"Postorder Traversal: ";
+   postorder(root);
+   return 0;
+}
