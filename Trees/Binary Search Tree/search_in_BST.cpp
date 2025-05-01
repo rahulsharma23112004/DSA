18d9fe9ce16492d2b4ebcfd22b5d0c2ba09cf876
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-01 07:25:17 +0530

    Create search_in_BST.cpp

---
+++ Trees/Binary Search Tree/search_in_BST.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* insertIntoBST(Node* root , int data)
+{
+    if(root==NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    if(data > root->data){
+        root->right = insertIntoBST(root->right , data);
+    }
+    else{
+        root->left = insertIntoBST(root->left , data);
+    }
+
+    return root;
+}
+
+void takeInput(Node* &root)
+{
+    int data ;
+    cin>>data;
+
+    while(data != -1){
+        root = insertIntoBST(root , data);
+        cin>>data;
+    }
+}
+
+Node* levelOrderTraversal(Node* root)
+{
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+bool searchInBST(Node* root , int x)
+{
+    Node* temp = root;
+    while(temp != NULL){
+        if(temp->data == x){
+            return true;
+        }
+
+        if(temp->data > x){
+            temp = temp->left;
+        }
+        else{
+            temp = temp->right ;
+        }
+    }
+    return false;
+}
+
+
+int main()
+{
+    Node* root = NULL;
+    cout<<"Enter the data for inserting in BST: "<<endl;
+    takeInput(root);
+    cout<<endl;
+
+    cout<<"Printing BST: "<<endl;
+    levelOrderTraversal(root);
+
+    cout<<endl;
+
+    if(searchInBST(root , 15)){
+        cout<<"Node found in BST."<<endl;
+    }
+    else{
+        cout<<"Node Not found in BST."<<endl;
+    }
+
+    return 0;
+}
