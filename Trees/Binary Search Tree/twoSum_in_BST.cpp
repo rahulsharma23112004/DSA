081d40abdd7f18b797654eb5b322298432cdb8ce
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-02 22:47:27 +0530

    Create twoSum_in_BST.cpp

---
+++ Trees/Binary Search Tree/twoSum_in_BST.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* insertIntoBST(Node* root , int data)
+{
+    if(root==NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    if(data > root->data){
+        root->right = insertIntoBST(root->right , data);
+    }
+    else{
+        root->left = insertIntoBST(root->left , data);
+    }
+
+    return root;
+}
+
+void takeInput(Node* &root)
+{
+    int data ;
+    cin>>data;
+
+    while(data != -1){
+        root = insertIntoBST(root , data);
+        cin>>data;
+    }
+}
+
+Node* levelOrderTraversal(Node* root)
+{
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+void inorder(Node* root , vector<int> &in){
+    if(root==NULL){
+        return;
+    }
+
+    inorder(root->left , in);
+    in.push_back(root->data);
+    inorder(root->right , in);
+}
+
+bool twoSum(Node* root , int target)
+{
+    vector<int> inorderVal ;
+    inorder(root , inorderVal);
+    int i =0 , j= inorderVal.size()-1;
+    
+    while(i<j){
+        int sum = inorderVal[i] + inorderVal[j];
+        if(sum==target){
+            return true;
+        }
+        else if(sum>target){
+           j--;
+        }
+        else{
+            i++ ;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    Node* root = NULL;
+    cout<<"Enter the data for inserting in BST: "<<endl;
+    takeInput(root);
+    cout<<endl;
+
+    cout<<"Printing BST: "<<endl;
+    levelOrderTraversal(root);
+
+    cout<<endl;
+    int target = 20;
+
+    if(twoSum(root, target)){
+        cout<<"Two nodes exist whose sum is equal to target"<<endl;
+    }
+    else{
+        cout<<"No two nodes exist."<<endl;
+    }
+    return 0;
+}
