8033d9d93a0173c5c850157378bb6722d6f55a01
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-06 17:19:30 +0530

    Create merge_two_max_heaps.cpp

---
+++ Trees/Heap Tree/merge_two_max_heaps.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void heapify(vector<int> &arr , int n , int i )
+{
+    int largest = i ;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if(left<n && arr[largest] < arr[left]){
+        largest = left;
+    }
+
+    if(right<n && arr[largest] < arr[right]){
+        largest = right;
+    }
+
+    if(largest != i){
+        swap(arr[largest] , arr[i]);
+        heapify(arr , n , largest);
+    }
+}
+
+vector<int> mergeHeaps(vector<int> &a , vector<int> &b , int n , int m){
+    vector<int> ans;
+    for(auto i: a){
+        ans.push_back(i);
+    }
+    for(auto i: b){
+        ans.push_back(i);
+    }
+
+    int size = ans.size() ;
+    for(int i = size/2 ; i>=0 ; i--){
+        heapify(ans , size , i);
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> a = {2 , 4 ,6 , 8, 10} ;
+    vector<int> b = {1 ,3,5 ,7 ,9};
+    vector<int> ans = mergeHeaps(a , b  , 5 , 5) ;
+    cout<<"Printing Merged Heap: "<<endl;
+    for(int  i = 0 ; i<ans.size() ;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
