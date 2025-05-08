5c8051f0f1aa124accee5ab41c014a1496a84893
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-08 18:26:00 +0530

    Create lcp_approach1.cpp

---
+++ Tries/lcp_approach1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &arr , int n){
+    string ans;
+
+    for(int i = 0 ;i<arr[0].length() ;i++){
+        char ch = arr[0][i];
+
+        bool match = true;
+
+        for(int j = 1; j<n ; j++){
+            if(arr[j].size() < i  || ch != arr[j][i]){
+                match = false;
+                break;
+            }
+        }
+
+        if(match == false){
+            break;
+        }
+        else{
+            ans.push_back(ch);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<string> arr = {"coding" , "codezen" , "code" , "coders"};
+    int n = arr.size();
+    string ans = longestCommonPrefix(arr , n);
+    cout<<"Longest Common Prefix among given strings is: "<<ans<<endl;
+    return 0;
+}
