be54bdcd296592625b842a2f76b81427680b28c7
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-08 18:26:45 +0530

    Create prefixSearch.cpp

---
+++ Tries/prefixSearch.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+class TrieNode
+{
+    public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+
+    TrieNode(char ch){
+        data = ch;
+        for(int i =0; i<26 ;i++){
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie
+{
+    public:
+    TrieNode* root ;
+    Trie(){
+        root = new TrieNode('\0');
+    }
+
+    void insertUtil(TrieNode* root , string word)
+    {
+        if(word.length() == 0) {
+            root->isTerminal = true;
+            return;
+        }
+
+        int index = word[0] - 'A';
+        TrieNode* child;
+
+        if(root->children[index] !=NULL){
+            child = root->children[index];
+        }
+        else{
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        insertUtil(child , word.substr(1));
+    }
+
+    void insertWord(string word){
+        insertUtil(root , word);
+    }
+
+    bool prefixUtil(TrieNode* root , string word)
+    {
+        if(word.length() == 0){
+            return true;
+        }
+
+        int index = word[0] -'A' ;
+        TrieNode* child;
+
+        if(root->children[index] != NULL){
+            child = root->children[index];
+        }
+        else{
+            return false;
+        }
+        return prefixUtil(child , word.substr(1));
+    }
+
+    bool prefixWord(string word){
+        return prefixUtil(root , word);
+    }
+};
+
+int main()
+{
+   Trie *t = new Trie();
+   t->insertWord("ARM");
+   t->insertWord("TIME");
+   t->insertWord("DO");
+
+   string word = "D" ;
+
+   if(t->prefixWord(word)){
+    cout<<"Prefix is Present."<<endl;
+   }
+   else{
+    cout<<"Prefix is Not present"<<endl;
+   }
+   return 0;
+}
