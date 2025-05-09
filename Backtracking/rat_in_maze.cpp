9e529298c7a93f7d8558f9f957acdfdc3e832f30
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-09 08:52:33 +0530

    Create rat_in_maze.cpp

---
+++ Backtracking/rat_in_maze.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(int newx, int newy, vector<vector<bool>> &vis, vector<vector<int>> &arr, int n)
+{
+    if ((newx >= 0 && newx < n) && (newy >= 0 && newy < n) && vis[newx][newy] != 1 && arr[newx][newy] == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void solve(int x, int y, vector<vector<int>> &arr, int n, vector<string> &ans, vector<vector<bool>> &vis, string path)
+{
+    if (x == n - 1 && y == n - 1)
+    {
+        ans.push_back(path);
+        return;
+    }
+    vis[x][y] = 1;
+
+    // Down:
+    if (isSafe(x + 1, y, vis, arr, n))
+    {
+        solve(x + 1, y, arr, n, ans, vis, path + 'D');
+    }
+
+    // Left:
+    if (isSafe(x , y-1, vis, arr, n))
+    {
+        solve(x , y-1, arr, n, ans, vis, path + 'L');
+    }
+
+    // Right:
+    if (isSafe(x, y+1, vis, arr, n))
+    {
+        solve(x, y+1, arr, n, ans, vis, path + 'R');
+    }
+
+    // Up:
+    if (isSafe(x - 1, y, vis, arr, n))
+    {
+        solve(x - 1, y, arr, n, ans, vis, path + 'U');
+    }
+
+    vis[x][y] = 0;
+}
+
+vector<string> searchMaze(vector<vector<int>> &arr, int n)
+{
+    vector<string> ans;
+    vector<vector<bool>> visited( n , vector<bool>(n,0));
+    string path = "";
+    if(arr[0][0] == 0){
+        return ans;
+    }
+    solve(0 , 0 , arr , n , ans , visited , path);
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> arr = { {1 , 0 , 0, 0} , {1 , 1 , 0 , 0} ,{1 , 1, 0 , 0} , {0 , 1 , 1 ,1}};
+    int n = arr.size();
+    vector<string> ans = searchMaze(arr , n);
+
+    for(string path:ans){
+        cout<<path<<endl;
+    }
+    return 0;
+    
+}
