65a2fa518b4b3cda60ac8086cd5a3e7ee3e5203e
Rahul Sharma <rahulsrma2311@gmail.com>
2025-05-11 08:30:35 +0530

    Create using_top_down.cpp

---
+++ Dynamic Programming/Minimum Cost Climbing Stairs/using_top_down.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int solve(vector<int> &cost , int n , vector<int> &dp)
+{
+    if(n == 0)
+    {
+        return cost[0];
+    }
+
+    if(n==1){
+        return cost[1];
+    }
+
+    if(dp[n] != -1){
+        return dp[n] ;
+    }
+
+    dp[n] = cost[n] + min(solve(cost , n-1 , dp) , solve(cost , n-2 , dp));
+    return dp[n] ;
+    
+}
+
+int minCost(vector<int> &cost)
+{
+    int n = cost.size() ;
+    vector<int> dp(n+1 , -1);
+    int ans = min(solve(cost , n-1 , dp) , solve(cost , n-2 , dp));
+    return ans;
+}
+
+int main()
+{
+    vector<int> cost = {10 , 15, 20} ;
+    int ans = minCost(cost) ;
+    cout<<"Minimum Cost of Climibing Stairs: "<<ans<<endl;
+    return 0; 
+}
