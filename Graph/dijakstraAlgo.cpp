f681fd1ef6f4074805768cea50b62fd02bcc7ebb
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-05-16 12:11:42 +0530

    Update dijakstraAlgo.cpp

--- Graph/dijakstraAlgo.cpp
+++ Graph/dijakstraAlgo.cpp
@@ -1,53 +1,79 @@
-implementation_using_adjacencyList.cpp
-
 #include<iostream>
 #include<unordered_map>
+#include<vector>
 #include<list>
+#include<limits.h>
+#include<set>
 using namespace std;
 
-class graph
+vector<int> dijkstra(vector<vector<int>> &vec , int vertices , int edges , int source)
 {
-    public:
-    unordered_map<int , list<int>> adj;
+    unordered_map<int , list<pair<int , int>>> adj;
+    for(int i = 0 ; i<edges;i++){
+        int u = vec[i][0];
+        int v = vec[i][1];
+        int w = vec[i][2];
 
-    void addEdge(int u , int v , bool direction)
-    {
-        adj[u].push_back(v);
-        if(direction==0){
-            adj[v].push_back(u);
-        }
+        adj[u].push_back(make_pair(v , w));
+        adj[v].push_back(make_pair(u , w)) ;
     }
 
-    void printAdjList()
-    {
-        for(auto i: adj){
-            cout<<i.first<<" -> ";
-            for(auto j: i.second){
-                cout<< j <<" , ";
+    vector<int> dist(vertices);
+    for(int i =0 ;i<vertices;i++){
+        dist[i] = INT_MAX ;
+    }
+    set<pair<int , int>>  st;
+
+    dist[source] = 0;
+    st.insert(make_pair(0 , source));
+
+    while(!st.empty()){
+        auto top = *(st.begin());
+
+        int nodeDistance = top.first ;
+        int topNode = top.second ;
+
+        st.erase(st.begin());
+
+        for(auto neighbour: adj[topNode]){
+            if(nodeDistance + neighbour.second < dist[neighbour.first]){
+                auto record = st.find(make_pair(dist[neighbour.first] , neighbour.first));
+
+                if(record != st.end()){
+                   st.erase(record);
+                }
+                dist[neighbour.first] = nodeDistance + neighbour.second ;
+                
+                st.insert(make_pair(dist[neighbour.first] , neighbour.first));
             }
-            cout<<endl;
         }
     }
-};
+    return dist;
+}
 
 int main()
 {
-    int n;
-    cout<<"Enter the number of nodes: "<<endl;
-    cin>>n;
+    int vertices = 5; 
+    int edges = 6;    
 
-    int m;
-    cout<<"Enter the number of edges: "<<endl;
-    cin>>m;
+    // Graph represented as edge list: {u, v, weight}
+    vector<vector<int>> vec = {
+        {0, 1, 4},
+        {0, 2, 1},
+        {2, 1, 2},
+        {1, 3, 1},
+        {2, 3, 5},
+        {3, 4, 3}
+    };
 
-    graph g;
-     
-    cout<<"Enter the edges connection of graph: "<<endl;
-    for(int i = 0; i<m ;i++){
-        int u , v;
-        cin>>u>>v ;
-        g.addEdge(u , v , 0);
+    int source = 0;
+
+    vector<int> distances = dijkstra(vec, vertices, edges, source);
+
+    cout << "Shortest distances from source node " << source << ":\n";
+    for (int i = 0; i < distances.size(); i++) {
+        cout << "Node " << i << " -> " << distances[i] << "\n";
     }
-    g.printAdjList();
+
     return 0;
 }
