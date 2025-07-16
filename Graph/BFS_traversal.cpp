d81d1dc6e6c6cd010a85fd7f799515db37c0a7da
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-07-16 22:14:26 +0530

    Update BFS_traversal.cpp

--- Graph/BFS_traversal.cpp
+++ Graph/BFS_traversal.cpp
@@ -9,7 +9,7 @@ void prepareAdjList(unordered_map<int , list<int>> &adjList , vector<pair<int, i
 {
     for(int i =0;i<edges.size();i++){
         int u = edges[i].first;
-        int v = edges[i].second;
+        int v = edges[i].second; 
 
         adjList[u].push_back(v);
         adjList[v].push_back(u);
