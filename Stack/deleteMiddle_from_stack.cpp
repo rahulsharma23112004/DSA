7577c12481f67d7f3acf29a1cd0dcb70f0edf653
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-07-25 21:57:45 +0530

    Update deleteMiddle_from_stack.cpp

--- Stack/deleteMiddle_from_stack.cpp
+++ Stack/deleteMiddle_from_stack.cpp
@@ -12,7 +12,7 @@ void solve(stack<int>&inputStack , int count , int size){
     int num = inputStack.top() ;
     inputStack.pop() ;  
 
-    //recursive call:
+    //recursive call: 
     solve(inputStack , count+1 , size);
 
     inputStack.push(num) ;
