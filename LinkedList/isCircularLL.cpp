da5aa64b5a35e3925cbb57d1941c97092039b8af
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-07-26 07:54:49 +0530

    Update isCircularLL.cpp

--- LinkedList/isCircularLL.cpp
+++ LinkedList/isCircularLL.cpp
@@ -14,7 +14,7 @@ class Node{
 
 void insertNode(Node* &tail , int element , int d)
 {
-    if(tail==NULL){
+    if(tail==NULL){ 
         Node* temp = new Node(d);
         tail = temp;
         temp->next = temp;
