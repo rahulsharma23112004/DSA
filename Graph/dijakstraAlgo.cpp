09a6e6e53b0eecbd5c731ea3e399887d57b067a2
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-08-04 18:12:22 +0530

    Update dijakstraAlgo.cpp

--- Graph/dijakstraAlgo.cpp
+++ Graph/dijakstraAlgo.cpp
@@ -11,7 +11,7 @@ vector<int> dijkstra(vector<vector<int>> &vec , int vertices , int edges , int s
     unordered_map<int , list<pair<int , int>>> adj;
     for(int i = 0 ; i<edges;i++){
         int u = vec[i][0];
-        int v = vec[i][1];
+        int v = vec[i][1];  
         int w = vec[i][2];
 
         adj[u].push_back(make_pair(v , w));
