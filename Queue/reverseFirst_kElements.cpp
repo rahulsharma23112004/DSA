7ce255011cbe384e80b309e9bf7c308089f7d6cf
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-08-14 20:56:27 +0530

    Update reverseFirst_kElements.cpp

--- Queue/reverseFirst_kElements.cpp
+++ Queue/reverseFirst_kElements.cpp
@@ -8,7 +8,7 @@ queue<int> modifyQueue(queue<int> &q , int k)
     stack<int> s;
 
     for(int i=0;i<k;i++){
-        int val = q.front();
+        int val = q.front();  
         q.pop();
         s.push(val);  
     }
