f1f5aa976d29201a0de5bf653d992009f85db9ef
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-08-20 08:20:18 +0530

    Update bellmanFord_algo.cpp

--- Graph/bellmanFord_algo.cpp
+++ Graph/bellmanFord_algo.cpp
@@ -13,7 +13,7 @@ int bellmanFord(int n , int m , int src, int dest,  vector<vector<int>> &edges)
         for(int j = 0 ;j <m ;j++){
             int u = edges[j][0];
             int v = edges[j][1];
-            int wt = edges[j][2];
+            int wt = edges[j][2];  
 
             if(dist[u] != 1e9 && (dist[u] + wt < dist[v])){
                 dist[v] = dist[u] + wt;
