f6e740ec16aeba5948ad0aadf7da1b8fc03e8d95
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-08-21 15:31:10 +0530

    Update sum_avg_of_arr.cpp

--- Array/sum_avg_of_arr.cpp
+++ Array/sum_avg_of_arr.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void inputArray(int arr[], int size)
 {
     cout<<"Enter te array elements: "<<endl;
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size;i++) 
     {
         cin>>arr[i];
     }
@@ -58,4 +58,5 @@ int main()
 
 
 }
+
 
