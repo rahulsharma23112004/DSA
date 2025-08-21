effb8c39361aacee05e6d09460fbadd583b389d7
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-08-21 15:30:54 +0530

    Update reverse_array.cpp

--- Array/reverse_array.cpp
+++ Array/reverse_array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void inputArray(int arr[], int size)
+void inputArray(int arr[], int size) 
 {
     cout<<"Enter te array elements: "<<endl;
     for(int i=0;i<size;i++)
@@ -43,6 +43,7 @@ int main()
     printArray(arr,size);
     printReverse(arr,size) ;
     return 0;
+
 
 
 }
