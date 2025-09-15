34705585d846499accf0320163041b0d06678503
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-09-15 21:35:50 +0530

    Update kruskal_algo.cpp

--- Graph/kruskal_algo.cpp
+++ Graph/kruskal_algo.cpp
@@ -15,7 +15,7 @@ void makeSet(vector<int> &parent , vector<int> &rank , int n)
     for(int i = 0 ;i<n ;i++){
         parent[i] = i;
         rank[i]= 0;
-    }
+    }  
 }
 
 int findParent(vector<int> &parent , int node)
