be49e6ae6ef12e7bd79ca76fbfe4b0a20e6e83fa
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-09-25 09:06:58 +0530

    Update prims_algo.cpp

--- Graph/prims_algo.cpp
+++ Graph/prims_algo.cpp
@@ -17,7 +17,7 @@ vector<pair<pair<int , int> , int>> calculatePrimsMST(int n, int m ,vector<pair<
         adj[v].push_back(make_pair(u,w));
     }
 
-    vector<int> key(n+1);
+    vector<int> key(n+1);  
     vector<bool> mst(n+1);  
     vector<int> parent(n+1);
 
