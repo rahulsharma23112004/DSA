4a67ef86b464bb460fba11a21f72448f230bcf7a
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2025-10-27 20:57:49 +0530

    Update bubbleSort_using_recursion.cpp

--- Array/bubbleSort_using_recursion.cpp
+++ Array/bubbleSort_using_recursion.cpp
@@ -8,7 +8,7 @@ void sortArray(int arr[] , int n)
     }
 
     for(int i=0;i<n-1;i++){
-        if(arr[i]>arr[i+1]){
+        if(arr[i]>arr[i+1]){  
             swap(arr[i],arr[i+1]);
         }
     }
@@ -25,3 +25,4 @@ int main()
     }
     return 0;
 }
+
