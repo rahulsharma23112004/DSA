0f38dc47b2a5402822255db6d1fab5c321dda8b6
Rahul Sharma <200810653+rahulsharma23112004@users.noreply.github.com>
2026-03-25 10:34:04 +0530

    Implement function to find largest element in array
    Fixes the loop condition to prevent out-of-bounds access.

---
+++ Array/Largest element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+       int maxVal= nums[0];
+
+        for(int i=1; i<=nums.size(); i++){
+            if(nums[i]>maxVal){
+                 maxVal = nums[i];
+            }
+           
+        }
+        return maxVal;
+
+    }
+};
